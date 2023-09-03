c64f803d3ec4b1f5889c94821b54f119562103d4
zemyblue <zemyblue@gmail.com>
2023-09-03 14:04:18 +0900

    chore: do refactoring test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S yarn node
 
+// This script should be executed after optimize a smart contract to artifacts directory (`cargo otpimize`)
+
 const { SigningFinschiaClient, makeLinkPath } = require("@finschia/finschia");
 const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
 const { calculateFee, GasPrice } = require("@cosmjs/stargate");
@@ -14,9 +16,123 @@ const alice = {
     address3: "link1dfyywjglcfptn72axxhsslpy8ep6wq7wujasma",
 }
 
+// instantiate
+const inits = [
+    {
+        label: "From deploy test.js",
+        msg: {
+            name: "A To Z Token",
+            symbol: "ATZ",
+            decimals: 6,
+            initial_balances: "1000000000",
+        },
+        admin: alice.address0,
+    },
+    {
+        label: "From deploy(2) test.js",
+        msg: {
+            name: "Second Token",
+            symbol: "SCD",
+            decimals: 6,
+            initial_balances: "1000000000",
+        },
+        admin: alice.address1,
+    },
+];
+
+class TokenClient {
+    constructor(client, codeId) {
+        this.client = client;
+        this.codeId = codeId;
+        this.gasPrice = GasPrice.fromString("0.025cony");
+    }
+
+    async instantiateContract(sender, initData) {
+        const instantiateFee = calculateFee(500_000, gasPrice);
+        const instRes = await this.client.instantiate(
+            sender, 
+            this.codeId, 
+            initData.msg, 
+            initData.label, 
+            instantiateFee,
+             {
+                memo: "Create a Test Token",
+                admin: initData.admin,
+            }
+        );
+        console.info(`Contract instantiated at ${instRes.contractAddress} in ${instRes.transactionHash}`);
+    
+        return instRes.contractAddress;
+    }
+
+    async transfer(contractAddress, sender, recipient, amount) {
+        const transferFee = calculateFee(150_000, gasPrice);
+        const transferMsg = {
+            transfer: {
+                recipient: recipient,
+                amount: amount,
+            },
+        };
+        const tranRes = await this.client.execute(sender, contractAddress, transferMsg, transferFee);
+        console.info(`Transfer txHash: ${tranRes.transactionHash}, events: ${JSON.stringify(tranRes.events)}`);
+
+        return tranRes.transactionHash;
+    }
+
+    async approve(contractAddress, sender, spender, amount, current_allowance) {
+        const approveFee = calculateFee(150_000, gasPrice);
+        const approveMsg = {
+            approve: {
+                spender: spender,
+                amount: amount,
+                current_allowance: current_allowance,
+            },
+        };
+        const approveRes = await this.client.execute(sender, contractAddress, approveMsg, approveFee);
+        console.info(`Approve txHash: ${approveRes.transactionHash}, events: ${JSON.stringify(approveRes.events)}`);
+
+        return approveRes.transactionHash;
+    }
+
+    async transferFrom(contractAddress, sender, owner, recipient, amount) {
+        const transferFromFee = calculateFee(150_000, gasPrice);
+        const transferFromMsg = {
+            transfer_from: {
+                owner: owner,
+                recipient: recipient,
+                amount: amount,
+            },
+        };
+        const tran2Res = await this.client.execute(sender, contractAddress, transferFromMsg, transferFromFee);
+        console.info(`TransferFrom txHash: ${tran2Res.transactionHash}, events: ${JSON.stringify(tran2Res.events)}`);
+
+        return tran2Res.transactionHash;
+    }
+}
+
+const gasPrice = GasPrice.fromString("0.025cony");
+
+async function instantiateContract(client, codeId, sender, initData) {
+    const instantiateFee = calculateFee(500_000, gasPrice);
+    const instRes = await client.instantiate(
+        sender, 
+        codeId, 
+        initData.msg, 
+        initData.label, 
+        instantiateFee,
+         {
+            memo: "Create a Test Token",
+            admin: initData.admin,
+        }
+    );
+    console.info(`Contract instantiated at ${instRes.contractAddress} in ${instRes.transactionHash}`);
+
+    return instRes.contractAddress;
+}
+
+
 async function main() {
     console.info("test");
-    const gasPrice = GasPrice.fromString("0.025cony");
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(alice.mnemonic, {
         hdPaths: [makeLinkPath(0), makeLinkPath(1), makeLinkPath(2), makeLinkPath(3)],
         prefix: "link",
@@ -30,70 +146,34 @@ async function main() {
     const uploadReceipt = await client.upload(alice.address0, wasm, uploadFee, "Upload standard contract");
     console.info(`Upload succeeded. Receipt: ${JSON.stringify(uploadReceipt)}`);
     
-    // instantiate
-    const init = {
-        label: "From deploy test.js",
-        msg: {
-            name: "A To Z Token",
-            symbol: "ATZ",
-            decimals: 6,
-            initial_balances: "1000000000",
-        },
-        admin: alice.address0,
-    };
+    const tokenClient = new TokenClient(client, uploadReceipt.codeId);
 
-    const instantiateFee = calculateFee(500_000, gasPrice);
-    const instRes = await client.instantiate(
-        alice.address0, 
-        uploadReceipt.codeId, 
-        init.msg, 
-        init.label, 
-        instantiateFee,
-         {
-            memo: "Create a Test Token",
-            admin: init.admin,
-        }
-    );
-    console.info(`Contract instantiated at ${instRes.contractAddress} in ${instRes.transactionHash}`);
-
-    const contractAddress = instRes.contractAddress;
-    // const contractAddress = "link1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgstwt0ss";
+    // instantiate 1
+    const contractAddress1 = await tokenClient.instantiateContract(alice.address0, inits[0]);
 
     // transfer
-    const transferFee = calculateFee(150_000, gasPrice);
-    const transferMsg = {
-        transfer: {
-            recipient: alice.address1,
-            amount: "1000",
-        },
-    };
-    const tranRes = await client.execute(alice.address0, contractAddress, transferMsg, transferFee);
-    console.info(`Transfer txHash: ${tranRes.transactionHash}, events: ${JSON.stringify(tranRes.events)}`);
+    await tokenClient.transfer(contractAddress1, alice.address0, alice.address1, "1000");
 
     // approve
-    const approveFee = calculateFee(150_000, gasPrice);
-    const approveMsg = {
-        approve: {
-            spender: alice.address2,
-            amount: "1000000",
-            current_allowance: "0",
-        },
-    };
-    const approveRes = await client.execute(alice.address0, contractAddress, approveMsg, approveFee);
-    console.info(`Approve txHash: ${approveRes.transactionHash}, events: ${JSON.stringify(approveRes.events)}`);
+    await tokenClient.approve(contractAddress1, alice.address0, alice.address2, "1000000", "0");
 
     // transferFrom
-    const transferFromFee = calculateFee(150_000, gasPrice);
-    const transferFromMsg = {
-        transfer_from: {
-            owner: alice.address0,
-            recipient: alice.address3,
-            amount: "100000",
-        },
-    };
-    const tran2Res = await client.execute(alice.address2, contractAddress, transferFromMsg, transferFromFee);
-    console.info(`TransferFrom txHash: ${tran2Res.transactionHash}, events: ${JSON.stringify(tran2Res.events)}`);
+    await tokenClient.transferFrom(contractAddress1, alice.address2, alice.address0, alice.address3, "100000");
+
+    // instantiate 2
+    const contractAddress2 = await tokenClient.instantiateContract(alice.address1, inits[1]);
+
+    // transfer to contractAddress2
+    await tokenClient.transfer(contractAddress1, alice.address0, contractAddress2, "1000");
+
+    // transfer to native module (x/foundation)
+    // grpcurl -plaintext -d '{"name":"foundation"}' 127.0.0.1:9090 cosmos.auth.v1beta1.Query/ModuleAccountByName
+    // address: link190vt0vxc8c8vj24a7mm3fjsenfu8f5yxxj76cp
+    const foundationAddress = "link190vt0vxc8c8vj24a7mm3fjsenfu8f5yxxj76cp";
+    await tokenClient.transfer(contractAddress1, alice.address0, foundationAddress, "10000");
+
 }
+
 
 main().then(
     () => {
